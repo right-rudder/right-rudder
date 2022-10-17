5e974b6cf03ece0fe6a84c40a4bd60108a6ea409
Tim Jedrek <tim@timjedrek.com>
2022-10-17 10:12:27 -0500

    create four forces of flight animation

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,6 +12,17 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      keyframes: {
+        wiggle: {
+          '0%': { transform: 'translateX(-10rem) translateY(8rem) scale(.6)', opacity: '0' },
+          '10%': { transform: 'rotate(0deg) translateY(0rem) scale(1)', opacity: '1'},
+          '25%': { transform: 'rotate(7deg)'},
+          '40%': { transform: 'rotate(5deg)'},
+          '55%': { transform: 'rotate(7deg)'},
+          '70%': { transform: 'rotate(0deg) translateY(0rem)', opacity: '1'},
+          '100%': { transform: 'rotate(-30deg) translateX(75rem) scale(.5)', opacity: '0'  },
+        },
+      }
     },
   },
   plugins: [
