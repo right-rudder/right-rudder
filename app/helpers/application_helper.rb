798ef658221318e38a481964a19ce023c7cd2f64
Tim Jedrek <tim@timjedrek.com>
2022-11-14 20:21:59 -0600

    create keyword and description helper methods and start on OG/twitter image helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,24 @@ module ApplicationHelper
     content_for :title, text
   end
 
+  # Meta Description Changer
+  def meta_description(text)
+    content_for :meta_description, text
+  end
+
+  # Meta Keywords Changer
+  def meta_keywords(text)
+    content_for :meta_keywords, text
+  end
+
+  # OG Picture
+  def og_picture(text)
+    content_for :og_picture, text
+  end
+
+  # Twitter Picture
+  def twitter_picture(text)
+    content_for :twitter_picture, text
+  end
+
 end
