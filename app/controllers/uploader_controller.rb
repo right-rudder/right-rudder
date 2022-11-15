1cc92f5f6c51905f7f6944f7372f9d6d526f2448
Tim Jedrek <tim@timjedrek.com>
2022-11-15 01:02:14 -0600

    add notes

--- app/controllers/uploader_controller.rb
+++ app/controllers/uploader_controller.rb
@@ -1,3 +1,7 @@
+#https://medium.com/@kristina.kabosiene/manual-upload-rails-6-active-storage-tinymce-5-and-aws-s3-2f139c0b5df4
+#used create_and_upload instead of create_after_upload... had to look at ruby docs..
+#https://api.rubyonrails.org/classes/ActiveStorage/Blob.html
+
 class UploaderController < ApplicationController
   skip_forgery_protection
   def image
