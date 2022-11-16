9451b8e6b12ef8f20ae723f0d0588b5a502b04e3
Tim Jedrek <tim@timjedrek.com>
2022-11-16 16:58:01 -0600

    fix spelling error

--- app/models/notam.rb
+++ app/models/notam.rb
@@ -1,7 +1,7 @@
 class Notam < ApplicationRecord
   validates :status, inclusion: { :in => ['Published','Drafting']}
   validates :title, presence: true
-  validates :category, inclusion: { :in => ['Business and Marketing','Aviation and Flying', 'News and Announcement']}
+  validates :category, inclusion: { :in => ['Business and Marketing','Aviation and Flying', 'News and Announcements']}
   validates :notam_date, presence: true
   validates :author, presence: true
   validates :feature_image, presence: true, blob: { content_type: ['image/png', 'image/jpg', 'image/jpeg', 'image/webp'], size_range: 1..(2.megabytes) }
