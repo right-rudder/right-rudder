3b188083b7ebe9a5ef1e3d0fccf98b626ff9afd1
Tim Jedrek <tim@timjedrek.com>
2022-11-16 18:15:52 -0600

    remove notams from featured notams so we can create an assocation next

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_16_031801) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_17_001423) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,7 +43,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_16_031801) do
   end
 
   create_table "featured_notams", force: :cascade do |t|
-    t.string "notam"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
