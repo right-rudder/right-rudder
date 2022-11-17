e645814f28b49ba81c4a6f354c24a18e5cb00ff2
Tim Jedrek <tim@timjedrek.com>
2022-11-17 02:01:42 -0600

    create webinar link in footer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_17_024606) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_17_075418) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,6 +55,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_17_024606) do
     t.string "meta_keywords"
     t.string "tags"
     t.string "featured"
+  end
+
+  create_table "webinars", force: :cascade do |t|
+    t.string "title"
+    t.string "host"
+    t.date "webinar_date"
+    t.string "meta_description"
+    t.string "meta_keywords"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
