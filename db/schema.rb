ee339ab8d32d85834fa0dc6e04f9596b725e5981
Tim Jedrek <tim@timjedrek.com>
2022-11-17 17:43:09 -0600

    migrate database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_17_085557) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_17_233926) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,6 +52,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_17_085557) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "careers", force: :cascade do |t|
+    t.string "role"
+    t.string "location"
+    t.date "post_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
