a96614e92957afffb7ec7a4d3c7e9aad04df53dd
Tim Jedrek <tim@timjedrek.com>
2022-11-19 20:35:17 -0600

    limit to one admin only

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     get "admins", to: "devise/sessions#new"
   end
   
-  devise_for :admins
+  devise_for :admins, controllers: { registrations: "registrations", sessions: "sessions" }
 
 
   root "pages#home"
