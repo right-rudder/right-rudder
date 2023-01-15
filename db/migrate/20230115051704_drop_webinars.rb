dc026e36ba6cdc5ea93510d7100b114388fd89aa
Tim Jedrek <tim@timjedrek.com>
2023-01-14 23:17:59 -0600

    drop webinars table

---
+++ db/migrate/20230115051704_drop_webinars.rb
@@ -0,0 +1,5 @@
+class DropWebinars < ActiveRecord::Migration[7.0]
+  def change
+    drop_table :webinars
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_15_050052) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_15_051704) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -104,18 +104,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_15_050052) do
     t.string "slug"
     t.string "webinar"
     t.index ["slug"], name: "index_notams_on_slug", unique: true
-  end
-
-  create_table "webinars", force: :cascade do |t|
-    t.string "title"
-    t.string "host"
-    t.date "webinar_date"
-    t.string "meta_description"
-    t.string "meta_keywords"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "slug"
-    t.index ["slug"], name: "index_webinars_on_slug", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
