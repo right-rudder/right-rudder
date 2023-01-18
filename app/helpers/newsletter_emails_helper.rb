36764389234651344f61db0e2558646e428aea58
Tim Jedrek <tim@timjedrek.com>
2023-01-18 00:47:55 -0600

    create update google sheet functionality

--- app/helpers/newsletter_emails_helper.rb
+++ app/helpers/newsletter_emails_helper.rb
@@ -1,2 +1,24 @@
 module NewsletterEmailsHelper
+
+  def create_json_credentials(path)
+    credentials = {
+      "type": ENV['TYPE'],
+      "project_id": ENV['PROJECT_ID'],
+      "private_key_id": ENV['PRIVATE_KEY_ID'],
+      "private_key": ENV['PRIVATE_KEY'],
+      "client_email": ENV['CLIENT_EMAIL'],
+      "client_id": ENV['CLIENT_ID'],
+      "auth_uri": ENV['AUTH_URI'],
+      "token_uri": ENV['TOKEN_URI'],
+      "auth_provider_x509_cert_url": ENV['AUTH_PROVIDER_X509_CERT_URL'],
+      "client_x509_cert_url": ENV['CLIENT_X509_CERT_URL']
+    }
+  
+    File.open(path, 'w') do |f|
+      f.write(credentials.to_json)
+    end
+  
+    path
+  end
+  
 end
--- app/models/newsletter_email.rb
+++ app/models/newsletter_email.rb
@@ -1,6 +1,26 @@
+require 'google_drive'
+include NewsletterEmailsHelper
+
 class NewsletterEmail < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true
 
   validates_uniqueness_of :email
+
+  after_create :update_google_sheet
+
+  private
+
+  def update_google_sheet
+    path = create_json_credentials("config/googleconfig.json")
+    session = GoogleDrive::Session.from_service_account_key(path)
+    spreadsheet = session.spreadsheet_by_key("1-KvGu8x8Lqqsj7DhKPLhUPXWqFbcyUHYodhs0KdsZkw").worksheets[0]
+    
+    # Write the data from the new instance of the newsletter_emails model
+    spreadsheet.insert_rows(spreadsheet.num_rows + 1, [[self.email, self.name, self.created_at, "Newsletter Sign Up"]])
+
+    # Save the changes to the Google Sheet
+    spreadsheet.save
+  end
+
 end
