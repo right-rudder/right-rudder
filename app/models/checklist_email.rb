c761367e87c486375cac687413d299a8be106879
Tim Jedrek <tim@timjedrek.com>
2023-03-05 00:34:02 -0500

    add google sheet link to checklist download form

--- app/models/checklist_email.rb
+++ app/models/checklist_email.rb
@@ -1,7 +1,29 @@
+require 'google_drive'
+include NewsletterEmailsHelper
+
 class ChecklistEmail < ApplicationRecord
   validates :name, presence: true
   validates :phone, presence: true
   validates :email, presence: true
 
   validates_uniqueness_of :email
+
+  after_create :update_google_sheet
+
+  private
+
+  def update_google_sheet
+    create_json_credentials("config/googleconfig.json")
+    session = GoogleDrive::Session.from_service_account_key("config/googleconfig.json")
+
+    spreadsheet = session.spreadsheet_by_key("1-KvGu8x8Lqqsj7DhKPLhUPXWqFbcyUHYodhs0KdsZkw").worksheets[0]
+    
+    # Write the data from the new instance of the newsletter_emails model
+    spreadsheet.insert_rows(spreadsheet.num_rows + 1, [[self.name, self.email, "Checklist Sign Up", self.created_at]])
+
+    # Save the changes to the Google Sheet
+    spreadsheet.save
+
+    delete_file("config/googleconfig.json")
+  end
 end
