47bf1f693611c72067ab1c3f3c0b49a134a337ce
Tim Jedrek <tim@timjedrek.com>
2023-03-06 23:46:51 -0600

    add company

--- db/migrate/20230307054445_create_messages.rb
+++ db/migrate/20230307054445_create_messages.rb
@@ -4,6 +4,7 @@ class CreateMessages < ActiveRecord::Migration[7.0]
       t.string :name
       t.string :email
       t.string :phone
+      t.string :company
       t.text :content
 
       t.timestamps
