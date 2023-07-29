e9cd97f3830c4a24c2d151ad7cf4bd509a9ad696
Tim Jedrek <tim@timjedrek.com>
2023-07-29 12:02:10 -0500

    change URL

--- config/routes.rb
+++ config/routes.rb
@@ -66,6 +66,7 @@ Rails.application.routes.draw do
   get 'flight-school-website-design', to: 'pages#website_design', as: :website
   get 'search-engine-optimization', to: 'pages#search_engine_optimization' 
   get 'flight-school-seo', to: 'pages#search_engine_optimization', as: :seo
+  get 'flight-school-advertising-ppc', to: 'pages#pay_per_click_ads', as: :ppc
   get 'pay-per-click-ads', to: 'pages#pay_per_click_ads'
   #get 'video-creation', to: 'pages#video_creation'
   #get 'email-campaigns', to: 'pages#email_campaigns'
