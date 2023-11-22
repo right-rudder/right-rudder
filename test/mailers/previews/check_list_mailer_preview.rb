fc04dd53b884e4b6e2e7db9c04d72547c788b5b2
Tim Jedrek <tim@timjedrek.com>
2023-11-22 04:53:32 -0600

    revise checklist email

--- test/mailers/previews/check_list_mailer_preview.rb
+++ test/mailers/previews/check_list_mailer_preview.rb
@@ -3,7 +3,8 @@ class CheckListMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/check_list_mailer/check_list
   def check_list
-    CheckListMailer.check_list
+    checklist = ChecklistEmail.last
+    CheckListMailer.check_list(checklist.email, checklist.name)
   end
 
 end
