90a4fe155942fcd9f26ecc8292be7cab8952e0bb
Tim Jedrek <tim@timjedrek.com>
2023-11-22 05:06:49 -0600

    revise newsletter automailer

--- test/mailers/previews/news_letter_mailer_preview.rb
+++ test/mailers/previews/news_letter_mailer_preview.rb
@@ -3,7 +3,8 @@ class NewsLetterMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/news_letter_mailer/news_letter
   def news_letter
-    NewsLetterMailer.news_letter
+    newsletter = NewsletterEmail.last
+    NewsLetterMailer.news_letter(newsletter.email, newsletter.name)
   end
 
 end
