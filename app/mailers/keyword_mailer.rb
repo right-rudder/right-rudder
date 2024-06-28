a3b5e1cdcf8c338a104491faa3bdf9ed8f3f2d23
Tim Jedrek <tim@timjedrek.com>
2024-06-28 16:53:27 -0500

    change subject

--- app/mailers/keyword_mailer.rb
+++ app/mailers/keyword_mailer.rb
@@ -2,6 +2,6 @@ class KeywordMailer < ApplicationMailer
   def keyword(email, name)
     @name = name
 
-    mail(to: email, bcc: ["no-reply@rightrudder.marketing", "info@rightruddermarketing.com"], subject: "✈️ Flight School Marketing Cheat Sheet")
+    mail(to: email, bcc: ["no-reply@rightrudder.marketing", "info@rightruddermarketing.com"], subject: "✈️ Flight School Keywords")
   end
 end
