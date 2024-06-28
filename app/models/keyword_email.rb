f9a9867737a21d99fba72590865f345e26348b1d
Tim Jedrek <tim@timjedrek.com>
2024-06-28 16:08:53 -0500

    remove limit to one email per model for lead magnets

--- app/models/keyword_email.rb
+++ app/models/keyword_email.rb
@@ -9,7 +9,7 @@ class KeywordEmail < ApplicationRecord
   validates :phone, presence: true, format: { with: /\A\d{10}\z/, message: "must be a valid 10-digit phone number" }
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
 
-  validates_uniqueness_of :email
+  #validates_uniqueness_of :email
 
   after_create :update_google_sheet
 
