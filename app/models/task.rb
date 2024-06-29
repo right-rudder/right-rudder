0f5985400f2341b6892e29fc7e70c5853303a5ca
Diego <diegoyon@outlook.com>
2024-06-29 00:14:12 -0600

    fix new task and date ranges

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,10 +4,18 @@ class Task < ApplicationRecord
 
   scope :completed, -> { where(completed: true).order(updated_at: :desc) }
   scope :incompleted, -> { where(completed: false) }
-  scope :overdue, -> { incompleted.where("due_date < ?", Date.today).order(due_date: :asc).order(title: :asc) }
-  scope :due_today, -> { incompleted.where(due_date: Date.today).order(due_date: :asc).order(title: :asc) }
+  scope :overdue, -> { incompleted.where("due_date < ?", Date.current).order(due_date: :asc).order(title: :asc) }
+  scope :due_today, -> { incompleted.where(due_date: Date.current).order(due_date: :asc).order(title: :asc) }
   scope :due_tomorrow, -> { incompleted.where(due_date: Date.tomorrow).order(due_date: :asc).order(title: :asc) }
-  scope :due_later_this_week, -> { incompleted.where(due_date: Date.tomorrow + 1.day..Date.today.end_of_week).order(due_date: :asc).order(title: :asc) }
-  scope :due_next_week, -> { incompleted.where(due_date: Date.today.end_of_week + 1.day..Date.today.end_of_week + 1.week).order(due_date: :asc).order(title: :asc) }
-  scope :due_later, -> { incompleted.where("due_date > ?", Date.today.end_of_week + 1.week).order(due_date: :asc).order(title: :asc) }
+  scope :due_later_this_week, -> { incompleted.where(due_date: date_range_for_due_later_this_week).order(due_date: :asc).order(title: :asc) }
+  scope :due_next_week, -> { incompleted.where(due_date: Date.current.end_of_week + 1.day..Date.current.end_of_week + 1.week).order(due_date: :asc).order(title: :asc) }
+  scope :due_later, -> { incompleted.where("due_date > ?", Date.current.end_of_week + 1.week).order(due_date: :asc).order(title: :asc) }
+
+  def self.date_range_for_due_later_this_week
+    if Date.current.saturday? || Date.current.sunday?
+      []
+    else
+      incompleted.where(due_date: date_range_for_due_later_this_week).order(due_date: :asc).order(title: :asc)
+    end
+  end
 end
