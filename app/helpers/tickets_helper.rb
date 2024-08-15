a880d4ed8391b85c868eba67555bb0f06cc2defc
Diego <diegoyon@outlook.com>
2024-08-15 15:24:36 -0600

    make squawks expand and collapse

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -1,2 +1,13 @@
 module TicketsHelper
+  def ticket_categories = {
+    overdue: { tickets: @overdue_tickets, title: "Overdue", color: "text-red-500" },
+    due_today: { tickets: @due_today_tickets, title: "Due today" },
+    due_tomorrow: { tickets: @due_tomorrow_tickets, title: "Due tomorrow" },
+    due_later_this_week: { tickets: @due_later_this_week_tickets, title: "Due later this week" },
+    due_next_week: { tickets: @due_next_week_tickets, title: "Due next week" },
+    due_later_within_a_month: { tickets: @due_later_within_a_month_tickets, title: "Due later within a month" },
+    due_later: { tickets: @due_later_tickets, title: "Due later" },
+    no_due_date: { tickets: @no_due_date_tickets, title: "No due date" },
+    completed: { tickets: @completed_tickets, title: "Completed", color: "text-green-500" }
+  }
 end
---
+++ app/javascript/controllers/expandable_controller.js
@@ -0,0 +1,57 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = [
+    "overdueTickets",
+    "dueTodayTickets",
+    "dueTomorrowTickets",
+    "dueThisWeekTickets",
+    "dueLaterThisWeekTickets",
+    "dueNextWeekTickets",
+    "dueLaterWithinAMonthTickets",
+    "dueLaterTickets",
+    "noDueDateTickets",
+    "completedTickets",
+  ];
+
+  toggle(e) {
+    const targetId = e.target.id.concat("Target");
+    const targetElement = this[targetId];
+
+    if (targetElement) {
+      targetElement.classList.toggle("max-h-[80rem]");
+      targetElement.classList.toggle("max-h-0");
+    }
+
+    e.target.classList.toggle("-rotate-90");
+    e.target.classList.toggle("rotate-0");
+  }
+
+  expandAll() {
+    this.constructor.targets.forEach((target) => {
+      const targetElement = document.getElementById(target);
+      if (targetElement) {
+        const element = this[`${target}Target`];
+        element.classList.add("max-h-[80rem]");
+        element.classList.remove("max-h-0");
+
+        targetElement.classList.remove("-rotate-90");
+        targetElement.classList.add("rotate-0");
+      }
+    });
+  }
+
+  collapseAll() {
+    this.constructor.targets.forEach((target) => {
+      const targetElement = document.getElementById(target);
+      if (targetElement) {
+        const element = this[`${target}Target`];
+        element.classList.remove("max-h-[80rem]");
+        element.classList.add("max-h-0");
+
+        targetElement.classList.add("-rotate-90");
+        targetElement.classList.remove("rotate-0");
+      }
+    });
+  }
+}
