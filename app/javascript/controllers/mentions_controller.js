8c278fba677b10bef82d026c189587a32a6872b1
Diego <diegoyon@outlook.com>
2024-09-02 14:56:32 -0600

    send emails on mentions

--- app/javascript/controllers/mentions_controller.js
+++ app/javascript/controllers/mentions_controller.js
@@ -7,17 +7,23 @@ export default class extends Controller {
   connect() {
     this.tribute = new Tribute({
       values: async (text, cb) => {
-        const response = await get(`/users.json?query=${text}`);
-        if (response.ok) {
-          const users = await response.json;
-          cb(
-            users.map((user) => ({
-              key: `${user.first_name} ${user.last_name}`,
-              value: user.username,
-            }))
-          );
+        if (text.length > 0) {
+          const response = await get(`/users.json?query=${text}`);
+          if (response.ok) {
+            const users = await response.json;
+            cb(
+              users.map((user) => ({
+                key: `${user.first_name} ${user.last_name}`,
+                value: user.username,
+              }))
+            );
+          }
+        } else {
+          cb([]);
         }
       },
+      // Allows matching of words even if there's punctuation immediately after the trigger character
+      noMatchTemplate: () => "",
     });
     this.tribute.attach(this.element);
   }
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,6 +4,7 @@ class Comment < ApplicationRecord
   has_rich_text :content
   broadcasts_to :ticket
   after_create :send_notifications
+  after_create :notify_mentioned_users
   enum variant: { comment: 0, date_change_note: 1, completed_note: 2, reopened_note: 3 }
 
   delegate :account, to: :ticket
@@ -28,10 +29,19 @@ class Comment < ApplicationRecord
   private
 
   def send_notifications
-    users_to_notify = ticket.subscribers - [user]
+    users_to_notify = ticket.subscribers
     users_to_notify.each do |user_to_notify|
       Notification.create(recipient: user_to_notify, actor: user, action: "commented on", notifiable: ticket) if user_to_notify != user
-      CommentMailer.with(comment: self, user: user_to_notify, actor: user).new_comment.deliver_later(wait: 5.seconds)
+      CommentMailer.with(comment: self, user: user_to_notify, actor: user).new_comment.deliver_later(wait: 5.seconds) if user_to_notify != user
+    end
+  end
+
+  def notify_mentioned_users
+    mentioned_users = User.where(username: content.body.to_s.scan(/@([\w.]+)/).flatten)
+    mentioned_users.each do |mentioned_user|
+      Notification.create(recipient: mentioned_user, actor: user, action: "mentioned you in a comment", notifiable: ticket) if mentioned_user != user
+      CommentMailer.with(comment: self, user: mentioned_user, actor: user).mentioned.deliver_later(wait: 5.seconds) if mentioned_user != user
+      ticket.subscribers << mentioned_user unless ticket.subscribers.include?(mentioned_user)
     end
   end
 end
