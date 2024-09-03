15e81da50480ab75c3cc0ce4dce49266b759cb04
Diego <diegoyon@outlook.com>
2024-09-02 22:25:48 -0600

    improve performance through bullet gem

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,6 +2,7 @@ class Notification < ApplicationRecord
   belongs_to :recipient, class_name: "User"
   belongs_to :actor, class_name: "User"
   belongs_to :notifiable, polymorphic: true
+  delegate :account, to: :notifiable
 
   scope :unread, -> { where(read_at: nil) }
 
