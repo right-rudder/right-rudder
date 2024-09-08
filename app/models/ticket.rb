dedd13c928a516d0bad9d9e46ea42c2da1bd23ca
Diego <diegoyon@outlook.com>
2024-09-07 18:13:16 -0600

    reduce order code

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -32,14 +32,14 @@ class Ticket < ApplicationRecord
 
   scope :completed, -> { where(completed: true).order(updated_at: :desc) }
   scope :incompleted, -> { where(completed: false) }
-  scope :overdue, -> { incompleted.where("due_date < ?", Date.current).order(due_date: :asc).order(title: :asc) }
-  scope :due_today, -> { incompleted.where(due_date: Date.current).order(due_date: :asc).order(title: :asc) }
-  scope :due_tomorrow, -> { incompleted.where(due_date: Date.tomorrow).order(due_date: :asc).order(title: :asc) }
-  scope :due_later_this_week, -> { incompleted.where(due_date: date_range_for_due_later_this_week).order(due_date: :asc).order(title: :asc) }
-  scope :due_next_week, -> { incompleted.where(due_date: Date.current.end_of_week + 1.day..Date.current.end_of_week + 1.week).order(due_date: :asc).order(title: :asc) }
-  scope :due_later_within_a_month, -> { incompleted.where(due_date: Date.current.beginning_of_week + 2.weeks..Date.current + 1.month).order(due_date: :asc).order(title: :asc) }
-  scope :due_later, -> { incompleted.where("due_date > ?", Date.current + 1.month).order(due_date: :asc).order(title: :asc) }
-  scope :no_due_date, -> { incompleted.where(due_date: nil).order(title: :asc) }
+  scope :overdue, -> { incompleted.where("due_date < ?", Date.current).order(:due_date).order(:title) }
+  scope :due_today, -> { incompleted.where(due_date: Date.current).order(:due_date).order(:title) }
+  scope :due_tomorrow, -> { incompleted.where(due_date: Date.tomorrow).order(:due_date).order(:title) }
+  scope :due_later_this_week, -> { incompleted.where(due_date: date_range_for_due_later_this_week).order(:due_date).order(:title) }
+  scope :due_next_week, -> { incompleted.where(due_date: Date.current.end_of_week + 1.day..Date.current.end_of_week + 1.week).order(:due_date).order(:title) }
+  scope :due_later_within_a_month, -> { incompleted.where(due_date: Date.current.beginning_of_week + 2.weeks..Date.current + 1.month).order(:due_date).order(:title) }
+  scope :due_later, -> { incompleted.where("due_date > ?", Date.current + 1.month).order(:due_date).order(:title) }
+  scope :no_due_date, -> { incompleted.where(due_date: nil).order(:title) }
   scope :user_assigned_tickets, ->(user) { where(assigned_users: { id: user.id }) }
 
   def self.date_range_for_due_later_this_week
