3aba2d21a6ef649c5b1c55154d2a4202ce6e700f
Diego <diegoyon@outlook.com>
2024-10-06 11:23:11 -0600

    change email portal url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #SMTP config
-  config.action_mailer.default_url_options = { host: 'rightruddermarketing.com' }
+  config.action_mailer.default_url_options = { host: 'portal.rightruddermarketing.com' }
 
   config.action_mailer.delivery_method = :smtp
 
